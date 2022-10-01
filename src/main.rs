fd474b57068d06a9638a31d34cc6d6b8d35d16c4
DumbInACan <marco@santana.nyc>
2022-10-01 16:51:39 -0400

    stack overflow test added

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,9 @@ pub extern "C" fn _start() -> ! {
 
     rust_os::init();
 
-    fn stack_overflow() { stack_overflow(); }
-    
-    // trigger a stack overflow
-    stack_overflow();
+    /* trigger a stack overflow */
+    // fn stack_overflow() { stack_overflow(); }
+    // stack_overflow();
     
     /* trigger as page fault */
     // unsafe { *(0xacabbabe as *mut u64) = 42; };
