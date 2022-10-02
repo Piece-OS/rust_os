19c806977b4a48be85d5111be985040427f52711
marco <marco@santana.nyc>
2022-10-02 19:56:36 -0400

    minor bug fixes

--- src/serial.rs
+++ src/serial.rs
@@ -13,9 +13,9 @@ lazy_static! {
 #[doc(hidden)]
 pub fn _print(args: ::core::fmt::Arguments) {
     use core::fmt::Write;
-    use x86_64::instructions:interrupts;
+    use x86_64::instructions::interrupts;
 
-    interrupts::without_interrputs( || { 
+    interrupts::without_interrupts( || { 
         SERIAL1.lock()
             .write_fmt(args)
             .expect("Printing to serial failed");
