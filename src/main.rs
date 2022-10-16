fe8075ae32e37d73c0fd32664e173b5554d54065
DumbInACan <marco@santana.nyc>
2022-10-16 11:50:52 -0400

    decluttering on main

--- src/main.rs
+++ src/main.rs
@@ -27,52 +27,13 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
             println!("L4 Entry {}: {:?}", i, entry);
         }
     }
-    /* reading the page tables */
-    //use x86_64::registers::control::Cr3;
-    //let (level_4_page_table, _) = Cr3::read();
-
-    //println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
-
-
-    /* trigger as page fault */
-    // let ptr = 0xACABbabe as *mut u32
-    // let ptr = 0x205546 as *mut u32;
-    
-    /* read from a code page */
-    // unsafe { let x = *ptr; }
-    // println!("read worked");
-    
-    /* write to a code page */
-    // unsafe { *ptr = 42; };
-    // println!("write worked");
-    
-
-    /* trigger a stack overflow */
-    // fn stack_overflow() { stack_overflow(); }
-    // stack_overflow();
-    
-
-    /* invoke a breakpoint exception */
-    // x86_64::instructions::interrupts::int3();
 
     #[cfg(test)]
     test_main();
 
     println!("It did not crash!");
     rust_os::hlt_loop();
-    /* provoking deadlock */
-    /* to prevent deadlock
-     * we disable interrupts
-     *  during print
-     * disabling interrupts
-     *  should only be
-     *  used in wost-
-     *  case scenarios */
-    // loop {
-        // for _ in 0 .. 10000 {}
-        // use rust_os::print;
-        // print!("-");
-    // }
+
 }
 
 #[cfg(not(test))]
@@ -80,7 +41,6 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 fn panic(info: &PanicInfo)  -> ! {
     println!("{}", info);
     rust_os::hlt_loop();
-    //loop {}
 }
 
 #[cfg(test)]
