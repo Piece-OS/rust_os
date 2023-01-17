b706c532d11a9634482a973134b5f0988de734e9
marco <marco@santana.nyc>
2023-01-17 09:41:25 -0500

    WIP executor

--- executor.rs
+++ executor.rs
@@ -1,25 +1,6 @@
-use super::{Task, TaskId};
-
-use alloc::{collections::BTreeMap, sync::Arc};
-use core::task::{Context, Poll, Waker};
-use crossbeam_queue::ArrayQueue;
-
-pub struct Executor {
-    tasks: BTreeMap<TaskId, Task>,
-    task_queue: Arc<ArrayQueue<TaskId>>,
-    waker_cache: BTreeMap<TaskId, Waker>,
-}
+use core::task::{Context, Poll};
 
 impl Executor {
-
-    pub fn new() -> Self {
-        Executor {
-            tasks: BTreeMap::new(),
-            task_queue: Arc::new(ArrayQueue::new(100)),
-            waker_cache: BTreeMap::new(),
-        }
-    }
-
     pub fn spawn(&mut self, task: Task) {
         let task_id = task.id;
         if self.tasks.insert(task.id, task).is_some() {
@@ -27,7 +8,6 @@ impl Executor {
         }
         self.task_queue.push(task_id).expect("queue full")
     }
-
     fn run_ready_tasks(&mut self) {
         // destructure`self` to avoid borrow checker errors
         let Self { tasks, task_queue, waker_cache} = self;
